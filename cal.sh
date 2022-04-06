fe01000516477a71ee021a7b667f64d435f53bf5
root <root@LAPTOP-3NBSK13F.localdomain>
2022-04-06 16:55:59 +0900

    new file

--- cal.sh
+++ cal.sh
@@ -5,22 +5,19 @@ op=$1
 read n1 < num1.txt
 read n2 < num2.txt
 
-declare -a num
-num=($n1 $n2)
-
 if [ -n "$op" ]; then
 	case $op in
-		add) result=$((${num[0]}+${num[1]}));;
-		sub) result=$((${num[0]}-${num[1]}));;
-		div) result=$((${num[0]}/${num[1]}));;
-		mul) result=$((${num[0]}*${num[1]}));;
+		add) result=$(($n1+$n2));;
+		sub) result=$(($n1-$n2));;
+		div) result=$(($n1/$n2));;
+		mul) result=$(($n1*$n2));;
 
 		*) echo "Fatal error."
 			exit 1;;
 	esac
 
-	echo "num1 : ${num[0]}"
-	echo "num2 : ${num[1]}"
+	echo "num1 : $n1"
+	echo "num2 : $n2"
 	echo "op : ${op}"
 	echo "result : ${result}"
 else
@@ -30,21 +27,21 @@ else
 	do
 		op=${menu}
 		case $menu in
-			add) result=$((${num[0]}+${num[1]}))
+			add) result=$(($n1+$n2))
 				break;;
-			sub) result=$((${num[0]}-${num[1]}))
+			sub) result=$(($n1-$n2))
 				break;;
-			div) result=$((${num[0]}/${num[1]}))
+			div) result=$(($n1/$n2))
 				break;;
-			mul) result=$((${num[0]}*${num[1]}))
+			mul) result=$(($n1*$n2))
 				break;;
 
 			*)echo "Fatal error."
 				exit 1;;
 		esac
 	done
-	echo "num1 : ${num[0]}"
-	echo "num2 : ${num[1]}"
+	echo "num1 : $n1"
+	echo "num2 : $n2"
 	echo "op : ${op}"
 	echo "result : ${result}"
 fi
