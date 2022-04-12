b8f87366ac2d0a299498878e4a7cc5e09833b19d
Oh-Huiseok <rlatotquf45@naver.com>
2022-04-12 17:49:10 +0900

    hw v4

--- word_freq.py
+++ word_freq.py
@@ -3,19 +3,23 @@ import sys
 
 file = open(sys.argv[1], 'r')
 
-num=(sys.argv[2])
-print(num)
+num=int((sys.argv[2]))
+
 lines = file.readlines()
+
 Dic = {}
+
 for line in lines:
     line = line.strip()
     words = line.split()
     for word in words:
-        print(word)
         if (word in Dic):
             Dic[word] = Dic.pop(word) + 1
         else:
             Dic[word] = 1
 
-print(Dic)
-
+Dic = sorted(Dic.items(), key=lambda x: x[1], reverse=True)
+for n in range(0, num):
+    word=Dic[n].keys()
+    cnt=DIc[n].values()
+    print("%-10s%10d" %word %count)
